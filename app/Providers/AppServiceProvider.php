ea3322dccd417d5ba393b89e650f4a40bcd28a20
Marcus André Lima de Lima <marcus.lima@hotmail.com.br>
2025-12-29 23:39:28 -0300

    Fix: Remove Vite::prefetch e adiciona forceScheme HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
-use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,9 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Forçar HTTPS em produção
-        if (config('app.env') !== 'local') {
+        if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
         }
+
+        Vite::prefetch(concurrency: 3);
     }
 }
